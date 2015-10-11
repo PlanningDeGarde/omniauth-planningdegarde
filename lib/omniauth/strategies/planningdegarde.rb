c40d0496912b21e29f94513c99163a77c21901eb
Etienne Depaulis <etienne.depaulis@gmail.com>
2015-10-11 09:47:25 +0200

    Updated gem to use v2 API

--- lib/omniauth/strategies/planningdegarde.rb
+++ lib/omniauth/strategies/planningdegarde.rb
@@ -8,22 +8,23 @@ module OmniAuth
       option :provider_ignores_state, true
 
       option :client_options, {
-        site: 'https://app.planning-de-garde.fr',
+        site: 'https://app.planning-de-garde.fr/',
         authorize_url: '/oauth/authorize'
       }
 
-      uid { raw_info['user']['id'] }
+      uid { raw_info['id'] }
 
       info do
         {
-          email: raw_info['user']['email'],
-          first_name: raw_info['user']['first_name'],
-          last_name: raw_info['user']['last_name']
+          email: raw_info['email'],
+          first_name: raw_info['first_name'],
+          last_name: raw_info['last_name']
         }
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/api/v1/me.json').parsed
+
+        @raw_info ||= access_token.get('https://api.planning-de-garde.fr/v2/me.json').parsed
       end
     end
   end
